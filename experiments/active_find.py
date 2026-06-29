09feb063fb3b9532a59faf517e72414e8cadd695
JoseCunhaTeixeira <jose.cunha-teixeira@protonmail.com>
2026-06-29 09:35:33 +0200

    remove private experiments

---
+++ experiments/active_find.py
@@ -0,0 +1,188 @@
+from pathlib import Path
+
+from sigpipe.base import Acquisition, Coordinate
+from sigpipe.dataio import load_dispersion_curves
+from sigpipe.transformers import (
+    BidirectionalCorrelate,
+    Detrend,
+    Dispersion,
+    Load,
+    Mute,
+    Pad,
+    Pick,
+    Plot,
+    Save,
+    Stack,
+)
+
+data_dir = Path(
+    "//resinosa/PARTAGES_UNITES/DRT_CND/MATER-SHM_2026_BCH/data/20260529_essais_imagerie_invent/data_imagerie"
+)
+file_paths = [
+    data_dir / "20260529_134245_sin_70kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_134848_sin_85kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_135452_sin_100kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_140057_sin_115kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_140702_sin_130kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_141309_sin_145kHz_3c_hann_50V_sain1.gero",
+    data_dir / "20260529_134546_sin_70kHz_5c_hann_50V_sain1.gero",
+    data_dir / "20260529_135150_sin_85kHz_5c_hann_50V_sain1.gero",
+    data_dir / "20260529_135754_sin_100kHz_5c_hann_50V_sain1.gero",
+    data_dir / "20260529_140359_sin_115kHz_5c_hann_50V_sain1.gero",
+    data_dir / "20260529_141005_sin_130kHz_5c_hann_50V_sain1.gero",
+    data_dir / "20260529_141612_sin_145kHz_5c_hann_50V_sain1.gero",
+]
+
+modeled_curves = load_dispersion_curves(
+    file_paths=[
+        Path(
+            "/Users/JC287771/Documents/Work/data/2026-05-29_essai_imagerie_invent/models/disp_1.72mm.txt"
+        )
+    ]
+)
+
+SOURCES = (
+    # 1st ring
+    Coordinate(x=0.000, y=0.000, z=0),  # 00
+    Coordinate(x=0.0113, y=0.000, z=0),  # 01
+    Coordinate(x=2 * 0.0113, y=0.000, z=0),  # 02
+    Coordinate(x=3 * 0.0113, y=0.000, z=0),  # 03
+    Coordinate(x=4 * 0.0113, y=0.000, z=0),  # 04
+    Coordinate(x=5 * 0.0113, y=0.000, z=0),  # 05
+    Coordinate(x=6 * 0.0113, y=0.000, z=0),  # 06
+    Coordinate(x=7 * 0.0113, y=0.000, z=0),  # 07
+    Coordinate(x=8 * 0.0113, y=0.000, z=0),  # 08
+    Coordinate(x=9 * 0.0113, y=0.000, z=0),  # 09
+    Coordinate(x=10 * 0.0113, y=0.000, z=0),  # 10
+    Coordinate(x=11 * 0.0113, y=0.000, z=0),  # 11
+    Coordinate(x=12 * 0.0113, y=0.000, z=0),  # 12
+    Coordinate(x=13 * 0.0113, y=0.000, z=0),  # 13
+    Coordinate(x=14 * 0.0113, y=0.000, z=0),  # 14
+    Coordinate(x=15 * 0.0113, y=0.000, z=0),  # 15
+    # 2nd ring
+    Coordinate(x=0.000, y=0.012, z=0),  # 16
+    Coordinate(x=0.012, y=0.012, z=0),  # 17
+    Coordinate(x=0.024, y=0.012, z=0),  # 18
+    Coordinate(x=0.036, y=0.012, z=0),  # 19
+    Coordinate(x=0.048, y=0.012, z=0),  # 20
+    Coordinate(x=0.060, y=0.012, z=0),  # 21
+    Coordinate(x=0.072, y=0.012, z=0),  # 22
+    Coordinate(x=0.084, y=0.012, z=0),  # 23
+    Coordinate(x=0.096, y=0.012, z=0),  # 24
+    Coordinate(x=0.108, y=0.012, z=0),  # 25
+    Coordinate(x=0.120, y=0.012, z=0),  # 26
+    Coordinate(x=0.132, y=0.012, z=0),  # 27
+    Coordinate(x=0.144, y=0.012, z=0),  # 28
+    Coordinate(x=0.156, y=0.012, z=0),  # 29
+    Coordinate(x=0.168, y=0.012, z=0),  # 30
+    Coordinate(x=0.180, y=0.012, z=0),  # 31
+)
+RECEIVERS = SOURCES
+
+
+def run_pipeline(acquisitions, sources_to_load, receivers_to_load, folder_path):
+    pipeline = (
+        Load(
+            file_paths=file_paths,
+            data_type="gero_active",
+            acquisitions=acquisitions,
+            key="signal",
+            sources_to_load=sources_to_load,
+            receivers_to_load=receivers_to_load,
+        )
+        >> Detrend(method="constant")
+        >> Mute(vmin=1_000, vmax=2_500, taper=25)
+        >> BidirectionalCorrelate(method="cross")
+        >> Stack(method="phase_weighted", nu=2)
+        >> Pick(method="maximum")
+        >> Plot(folder_path=folder_path, normalize=True)
+        >> Save(folder_path=folder_path)
+        >> Pad(n=1_000, taper=25)
+        >> Dispersion(method="phase", fmin=0, fmax=2_000_000, vmin=0, vmax=7_000)
+        >> Pick(
+            method="maximum",
+            fmins=[20_000],
+            fmaxs=[200_000],
+            vmins=[0],
+            vmaxs=[2_500],
+            lbdmins=[0.0065],
+            lbdmaxs=[0.1],
+            labels=["A0"],
+        )
+        >> Plot(folder_path=folder_path, modeled_curves=modeled_curves)
+        >> Save(folder_path=folder_path)
+    )
+
+    pipeline.run()
+
+
+def run() -> None:
+
+    saving_dir = Path(
+        "/Users/JC287771/Documents/Work/data/2026-05-29_essai_imagerie_invent/results"
+    )
+
+    saving_dirs = [
+        saving_dir / "1/",
+        saving_dir / "2/",
+        saving_dir / "3/",
+        saving_dir / "4/",
+        saving_dir / "5/",
+        saving_dir / "6/",
+        saving_dir / "7/",
+        saving_dir / "8/",
+        saving_dir / "9/",
+        saving_dir / "10/",
+        saving_dir / "11/",
+        saving_dir / "12/",
+    ]
+
+    source_windows = [
+        [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 8, 9, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 12, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 13, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 14, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15],
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    ]
+
+    receiver_windows = [
+        [0, 1, 2, 3, 4],
+        [1, 2, 3, 4, 5],
+        [2, 3, 4, 5, 6],
+        [3, 4, 5, 6, 7],
+        [4, 5, 6, 7, 8],
+        [5, 6, 7, 8, 9],
+        [6, 7, 8, 9, 10],
+        [7, 8, 9, 10, 11],
+        [8, 9, 10, 11, 12],
+        [9, 10, 11, 12, 13],
+        [10, 11, 12, 13, 14],
+        [11, 12, 13, 14, 15],
+    ]
+
+    for sources_to_load, receivers_to_load, folder_path in zip(
+        source_windows, receiver_windows, saving_dirs, strict=False
+    ):
+        sources = tuple(SOURCES[i] for i in sources_to_load)
+        receivers = tuple(RECEIVERS[i] for i in receivers_to_load)
+        acquisitions = [Acquisition(source=source, receivers=receivers) for source in sources]
+
+        run_pipeline(
+            acquisitions=acquisitions,
+            sources_to_load=sources_to_load,
+            receivers_to_load=receivers_to_load,
+            folder_path=folder_path,
+        )
+
+    # pipeline_section = Load(
+    #     file_paths=[saving_dir / "disps.csv"],
+    #     data_type=DispersionCurves,
+    # ) >> PlotSection(folder_path=saving_dir)
+    # pipeline_section.run()
---
+++ experiments/passive.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+from sigpipe.base import Acquisition, Coordinate
+from sigpipe.transformers import (
+    Apodize,
+    Correlate,
+    Detrend,
+    Filter,
+    Load,
+    Normalize,
+    Plot,
+    Slice,
+    Stack,
+    Whiten,
+)
+
+data_dir = Path("//drtvini/Echange/JCU/exemple donnees passives/data_passif_sain")
+file_paths = [
+    data_dir / "20250328_162747_1_1.gero",
+    data_dir / "20250328_162751_1_2.gero",
+    data_dir / "20250328_162756_1_3.gero",
+    data_dir / "20250328_162800_1_4.gero",
+    data_dir / "20250328_162804_1_5.gero",
+]
+
+saving_dir = Path("/Users/JC287771/Documents/Work/data/2026-05-09_passive_tests/results")
+
+source = Coordinate(x=0, y=0, z=0)
+receivers = (
+    ### PZT
+    Coordinate(x=0, y=0, z=0),
+    Coordinate(x=1, y=0, z=0),
+    Coordinate(x=2, y=0, z=0),
+    Coordinate(x=3, y=0, z=0),
+    Coordinate(x=-3, y=0, z=0),
+    Coordinate(x=-2, y=0, z=0),
+    Coordinate(x=-1, y=0, z=0),
+    ### FIBER
+    Coordinate(x=0, y=0, z=0),
+    Coordinate(x=1, y=0, z=0),
+    Coordinate(x=2, y=0, z=0),
+    Coordinate(x=3, y=0, z=0),
+    Coordinate(x=-3, y=0, z=0),
+    Coordinate(x=-2, y=0, z=0),
+    Coordinate(x=-1, y=0, z=0),
+)
+receivers_to_load = [
+    ### PZT
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    ### FIBER
+    # 7,
+    # 8,
+    # 9,
+    # 10,
+    # 11,
+    # 12,
+    # 13,
+]
+receivers = tuple(receivers[i] for i in receivers_to_load)
+acquisition = Acquisition(source=source, receivers=receivers)
+
+pipeline = (
+    Load(
+        file_paths=file_paths,
+        data_type="gero_passive",
+        acquisition=acquisition,
+        sort=True,
+        receivers_to_load=receivers_to_load,
+    )
+    >> Detrend(method="constant")
+    >> Detrend(method="linear")
+    >> Filter(method="iir", fmin=10_000, fmax=20_000, order=4)
+    >> Slice(segment_duration=0.002, segment_step=0.002)
+    >> Whiten(method="onebit_apod", fmin=10_000, fmax=20_000, taper_width_Hz=1_000)
+    >> Normalize(method="onebit")
+    >> Apodize(method="hanning", frac=0.1)
+    >> Correlate(method="cross", virtual_source_index=0)
+    >> Stack(method="phase_weighted", nu=2)
+    >> Plot(folder_path=saving_dir, normalize=True)
+)
+
+
+def run() -> None:
+    pipeline.run()
